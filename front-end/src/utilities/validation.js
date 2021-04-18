18bc8c6dd3e2f40a9a9e8b220ea5da151add5581
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2021-04-18 22:44:54 +0600

    add method to validation.js

--- front-end/src/utilities/validation.js
+++ front-end/src/utilities/validation.js
@@ -10,5 +10,17 @@ export default {
       }
     }
     return isObjectEmpty;
+  },
+  isAllPropertyOfObjectContainValue(object){
+    let allPropertyHasValue=true;
+    if(object!=null || object != undefined) {
+      for (let key in object) {
+        if (object[key] === "") {
+          allPropertyHasValue = false;
+          break;
+        }
+      }
+      return allPropertyHasValue;
+    }
   }
 }
