286dde936a665846d2fc3dced1b0bfb5d0ba5dc3
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2021-04-18 13:07:50 +0600

    init front end and entity

---
+++ back-end/src/main/java/com/salahin/todo/entities/PeripheralEntity.java
@@ -0,0 +1,44 @@
+/**
+ * Created By: Md. Nazmus Salahin
+ * Created Date: 23-Jan-2021
+ * Time: 1:10 PM
+ * Modified By:
+ * Modified date:
+ * (C) CopyRight Salahin ltd.
+ */
+
+package com.salahin.todo.entities;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "peripheral")
+public class PeripheralEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "uid", nullable = false)
+	private Long uid;
+	
+	@Column(name="vendor")
+	private String vendor;
+
+	@Column(name = "created_date")
+	private Date createdDate;
+	
+	@Column(name="status")
+	private boolean status;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private GatewayEntity gateway;
+	
+
+}
