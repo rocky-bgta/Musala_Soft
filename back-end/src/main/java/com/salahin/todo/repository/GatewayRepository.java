41a30e549ec9f1ca9fdb069731c901b3155837fe
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2021-04-18 13:23:30 +0600

    create model and repository

---
+++ back-end/src/main/java/com/salahin/todo/repository/GatewayRepository.java
@@ -0,0 +1,20 @@
+/**
+ * Created By: Md. Nazmus Salahin
+ * Created Date: 23-Jan-2021
+ * Time: 4:19 AM
+ * Modified By:
+ * Modified date:
+ * (C) CopyRight Salahin ltd.
+ */
+
+package com.salahin.todo.repository;
+
+import com.salahin.todo.entities.GatewayEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface GatewayRepository extends JpaRepository<GatewayEntity, UUID> {
+}
---
+++ back-end/src/main/java/com/salahin/todo/repository/PeripheralRepository.java
@@ -0,0 +1,18 @@
+/**
+ * Created By: Md. Nazmus Salahin
+ * Created Date: 23-Jan-2021
+ * Time: 4:19 AM
+ * Modified By:
+ * Modified date:
+ * (C) CopyRight Salahin ltd.
+ */
+
+package com.salahin.todo.repository;
+
+import com.salahin.todo.entities.PeripheralEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeripheralRepository extends JpaRepository<PeripheralEntity, Long> {
+}
